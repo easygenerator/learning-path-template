3caee7e0212bdd1e59c382e84437fabfe9cd1b06
NadiaGamula <nadia.zakharova@gmail.com>
2015-07-06 11:06:18 +0300

    Add versions

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,29 @@ var gulp = require('gulp'),
     minifyCss = require('gulp-minify-css'),
     uglify = require('gulp-uglify'),
     useref = require('gulp-useref'),
+    eventStream = require('event-stream'),
     gulpif = require('gulp-if'),
-    output = ".output";
+    output = ".output",
+    buildVersion = +new Date();
 
-gulp.task('build', ['clean', 'build-app'], function () {
-});
+    function addBuildVersion() {
+        return eventStream.map(function (file, callback) {
+            var fileContent = String(file.contents);
+            fileContent = fileContent
+                .replace(/(\?|\&)v=([0-9]+)/gi, '') // remove build version
+                .replace(/\.(jpeg|jpg|png|gif|css|js|html|eot|svg|ttf|woff)([?])/gi, '.$1?v=' + buildVersion + '&') // add build version to resource with existing query param
+                .replace(/\.(jpeg|jpg|png|gif|css|js|html|eot|svg|ttf|woff)([\s\"\'\)])/gi, '.$1?v=' + buildVersion + '$2') // add build version to resource without query param
+                .replace(/urlArgs: 'v=buildVersion'/gi, 'urlArgs: \'v=' + buildVersion + '\''); // replace build version for require config
+            file.contents = new Buffer(fileContent);
+            callback(null, file);
+        });
+    };
 
 gulp.task('clean', function (cb) {
     del([output], cb);
 });
 
-gulp.task('build-app', ['clean'], function () {
+gulp.task('build', ['clean'], function () {
     var assets = useref.assets();
 
     gulp.src('index.html')
@@ -22,6 +34,7 @@ gulp.task('build-app', ['clean'], function () {
         .pipe(gulpif('*.css', minifyCss()))
         .pipe(assets.restore())
         .pipe(useref())
+        .pipe(addBuildVersion())
         .pipe(gulp.dest(output));
 
     return gulp.src('css/font/**')
