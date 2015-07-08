c69d09f9e164f171e0f20933acd5ac9fa72db337
NadiaGamula <nadia.zakharova@gmail.com>
2015-07-08 16:12:53 +0300

    Add course thumbnail

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 ﻿(function () {
+    var dataUrl = 'data/data.json',
+        manifestUrl = '/manifest.json',
+        defaultCourseThumbnailUrl = 'img/default.png';
 
     var viewModel = {
         isError: ko.observable(false),
@@ -7,15 +10,36 @@
     };
 
     viewModel.init = function () {
-        var dataUrl = 'data/data.json';
-
         return $.getJSON(dataUrl).then(function (data) {
-            viewModel.title = data.title;
-            viewModel.courses = data.courses;
+            return getThumbnailUrls(data.courses).then(function () {
+                viewModel.title = data.title;
+                viewModel.courses = data.courses;
+            });
         }).fail(function () {
             viewModel.isError(true);
         });
     };
+
+    function getThumbnailUrls(courses) {
+        var arrayPromises = [];
+        $.each(courses, function (index, course) {
+            if (course.link) {
+                arrayPromises.push(getThumbnailUrl(course.link).then(function (thumbnailUrl) {
+                    course.thumbnailUrl = thumbnailUrl;
+                }));
+            }
+        });
+
+        return $.when.apply($, arrayPromises);
+    }
+
+    function getThumbnailUrl(link) {
+        return $.getJSON(link + manifestUrl).then(function (manifest) {
+            return manifest.thumbnail ? link + '/' + manifest.thumbnail : defaultCourseThumbnailUrl;
+        }).fail(function () {
+            return defaultCourseThumbnailUrl;
+        });
+    }
 
     viewModel.init().always(function () {
         $(document).ready(function () {
